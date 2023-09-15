88b20344c3facdb2f31a295f83d997d4096f5891
Suslov-MD <maksuslov@gmail.com>
2023-09-15 10:54:14 +0600

    feat: added config and mongo module

--- apps/ai-codenames-backend/src/app/app.module.ts
+++ apps/ai-codenames-backend/src/app/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoConfig } from '../config/db-connect.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'envs/.backend.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
